9e9f510920ad2ffea8f174f2e0336afc816eb0de
u-Kuro <rcdupitas@gmail.com>
2023-06-01 21:37:39 +0800

    finished connected most filter functionality, need to add warning info and color icon next

--- src/components/Anime/Fixed/AnimeOptionsPopup.svelte
+++ src/components/Anime/Fixed/AnimeOptionsPopup.svelte
@@ -4,12 +4,12 @@
 <div id="anime-options">
     <div id="anime-options-container">
         <span id="anime-option-openpopup">Open Anime</span>
-        <a
+        <!-- <a
             href="javascript:;"
             target="_blank"
             rel="noopener noreferrer"
             id="anime-option-gotolink">Open In Anilist</a
-        >
+        > -->
         <span id="anime-option-copy-title">Copy Title</span>
         <span id="anime-option-hideshow">---</span>
     </div>
