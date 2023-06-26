3f2f0deb6e565e828f791f381c4b88c5793ecf22
u-Kuro <rcdupitas@gmail.com>
2023-06-26 17:56:11 +0800

    trying request progress load

--- docs/src/js/caching.js
+++ docs/src/js/caching.js
@@ -11,63 +11,50 @@ const cacheRequest = (url) => {
             resolve(loadedUrls[url])
         } else {
             ++runningRequest
-            fetch(url, {
-                headers: {
-                    'Cache-Control': 'max-age=31536000, immutable'
-                }
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(res.statusText);
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.responseType = "text";
+            xhr.setRequestHeader('Cache-Control', 'max-age=31536000, immutable');
+            xhr.onprogress = function (event) {
+                if (event.lengthComputable) {
+                    let loaded = event.loaded;
+                    let total = event.total;
+                    let progress = (loaded / total) * 100;
+                    console.log(progress, loaded, total, url)
+                    if (runningRequest < 2) { // Just One Running Request
+                        minProgress = progress
+                    } else if (progress < minProgress) { // More than 1 Request
+                        minProgress = progress
                     }
-                    let contentLength = parseFloat(response.headers.get('content-length'));
-                    let loaded = 0;
-                    if (isNaN(contentLength)) return
-                    return new Response(
-                        new ReadableStream({
-                            start(controller) {
-                                const reader = response.body.getReader();
-                                read();
-                                function read() {
-                                    reader.read()
-                                        .then((progressEvent) => {
-                                            console.log(loaded, progressEvent?.value?.byteLength, progressEvent?.value, contentLength, progressEvent, url)
-                                            if (progressEvent.done) {
-                                                controller.close();
-                                                return;
-                                            }
-                                            console.log(loaded, progressEvent.value.byteLength, contentLength, progressEvent, url)
-                                            loaded += progressEvent.value.byteLength;
-                                            let progress = (loaded / contentLength) * 100
-                                            if (runningRequest < 2) { // Just One Running Request
-                                                minProgress = progress
-                                            } else if (progress < minProgress) { // More than 1 Request
-                                                minProgress = progress
-                                            }
-                                            requestProgress.set(minProgress)
-                                            controller.enqueue(progressEvent.value);
-                                            read();
-                                        })
-                                }
-                            }
-                        })
-                    );
-                })
-                .then(response => response.blob())
-                .then(blob => {
-                    --runningRequest
+                    requestProgress.set(minProgress)
+                }
+            };
+            xhr.onload = () => {
+                --runningRequest
+                if (xhr.status === 200) {
                     if (runningRequest < 1) { // All requests have finished
                         minProgress = 100
                         requestProgress.set(minProgress)
                     }
-                    let bloburl = URL.createObjectURL(blob);
-                    loadedUrls[url] = bloburl
-                    resolve(bloburl);
-                })
-                .catch((error) => {
-                    --runningRequest
-                    reject(new Error(error))
-                })
+                    const contentType = xhr.getResponseHeader('Content-Type');
+                    let responseData;
+                    if (xhr.responseType === 'text' || xhr.responseType === '') {
+                        responseData = xhr.responseText;
+                    } else {
+                        responseData = xhr.response;
+                    }
+                    const blob = new Blob([responseData], { type: contentType });
+                    const url = URL.createObjectURL(blob);
+                    resolve(url);
+                } else {
+                    reject(new Error(xhr.status));
+                }
+            };
+            xhr.onerror = (error) => {
+                --runningRequest
+                reject(new Error(error));
+            };
+            xhr.send();
         }
     })
 }
