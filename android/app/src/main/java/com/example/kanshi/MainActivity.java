b7cf119b5884d1fba7968965192400428dde37a8
u-Kuro <rcdupitas@gmail.com>
2023-12-01 18:47:07 +0800

    fixed year filter

--- android/app/src/main/java/com/example/kanshi/MainActivity.java
+++ android/app/src/main/java/com/example/kanshi/MainActivity.java
@@ -75,7 +75,7 @@ import androidx.core.content.FileProvider;
 import androidx.core.splashscreen.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
-    public final int appID = 223;
+    public final int appID = 224;
     public boolean webViewIsLoaded = false;
     public boolean permissionIsAsked = false;
     public SharedPreferences prefs;
--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
-const version = 223;
+const version = 224;
 export default async function getWebVersion() {
     try {
         let path = window.location.pathname;
