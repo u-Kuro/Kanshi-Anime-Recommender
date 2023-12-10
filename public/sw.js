88b49e73a87a4a0af49413d1c1f1b068117035f8
u-Kuro <rcdupitas@gmail.com>
2023-12-11 01:19:05 +0800

    check serviceworker in vercel

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,9 @@
-self.addEventListener("install",e=>{}),self.addEventListener("fetch",e=>e.request.url!==self.location.href&&e.respondWith(fetch(e.request)))
+self.addEventListener("install", e => { }), self.addEventListener("fetch", e => {
+    console.log(self)
+    console.log(self.location)
+    console.log(self.location.href)
+    console.log(e);
+    console.log(e.request);
+    console.log(e.request.url);
+    e.request.url !== self.location.href && e.respondWith(fetch(e.request))
+})
