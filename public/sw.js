0675e793ec7314861d6718b0e0560e8a3c234703
u-Kuro <rcdupitas@gmail.com>
2023-12-11 01:33:10 +0800

    check serviceworker in vercel

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,4 @@
-self.addEventListener("install",e=>{}),self.addEventListener("fetch",e=>{"https://kanshi.vercel.app/_vercel/insights/script.js"!==e.request.url&&e.respondWith(fetch(e.request).catch(e=>{}))});
+self.addEventListener("install",e=>{}),self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(e=>{
+    console.log(e);
+    return e.response
+}))});
