a279a11f3a24cdf2012266afa32229bcafdc330f
u-Kuro <rcdupitas@gmail.com>
2023-12-11 01:36:58 +0800

    check serviceworker in vercel

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,8 @@
-self.addEventListener("install",e=>{}),self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(e=>{
-    console.log(e);
-    return e.response
-}))});
+self.addEventListener("install", e => { }), self.addEventListener("fetch", e => {
+    e.respondWith(fetch(e.request).catch(e => {
+        return new Response('Error: ' + error.message, {
+            status: 500,
+            statusText: 'Internal Server Error'
+        });
+    }))
+});
